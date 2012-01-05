96cbf5bf18ceb845b74dbfc9d17bfc02899fb033
Rhys Weatherley <rhys.weatherley@gmail.com>
2012-01-05 19:17:30 +1000

    Update copyrights for the new year

--- android/TVGuide/src/com/southernstorm/tvguide/TvChannel.java
+++ android/TVGuide/src/com/southernstorm/tvguide/TvChannel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011  Southern Storm Software, Pty Ltd.
+ * Copyright (C) 2011,2012  Southern Storm Software, Pty Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
